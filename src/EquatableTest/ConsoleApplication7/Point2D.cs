660a902339ab9b9ac94aad70c94b655ce495912c
BogdanPetre <bogdan.sgs4@gmail.com>
2016-01-09 23:05:56 +0000

    Demo of performance using IEquatable.

---
+++ src/EquatableTest/ConsoleApplication7/Point2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+	/*public struct Point2D
+	{
+		public int X;
+		public int Y;
+	}*/
+
+	public struct Point2D : IEquatable<Point2D>
+	{
+		public int X;
+		public int Y;
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Point2D))
+				return false;
+			Point2D other = (Point2D)obj;
+			return (X == other.X) && (Y == other.Y);
+		}
+
+		public bool Equals(Point2D other)
+		{
+			return (X == other.X) && (Y == other.Y);
+		}
+		
+		public static bool operator ==(Point2D a, Point2D b)
+		{
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(Point2D a, Point2D b)
+		{
+			return !(a == b);
+		}
+	}
+}
---
+++ src/EquatableTest/ConsoleApplication7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int size = 10 * 1000 * 1000;
+			var sw = Stopwatch.StartNew();
+			var points = new List<Point2D>(size);
+			Checkpoint(sw);
+
+			for (int i = 0; i < size; i++)
+			{
+				points.Add(new Point2D { X = 2 * i, Y = i });
+			}
+			Checkpoint(sw);
+
+			var r = new Point2D { X = 1, Y = 10 };
+			if(points.Contains(r))
+				Console.WriteLine("Punctul a fost gasit");
+			else
+				Console.WriteLine("Punctul nu exista");
+			Checkpoint(sw);
+
+			Console.ReadLine();
+		}
+
+		static void Checkpoint(Stopwatch sw)
+		{
+			sw.Stop();
+			Console.WriteLine("ticks = {0}", sw.ElapsedTicks);
+			sw.Restart();
+		}
+	}
+}
